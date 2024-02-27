d0cd1f0bb2cd54781b78e2698a09a16de7a3e39f
ThommyShelby9 <merluxsolitaire@gmail.com>
2024-02-27 12:13:05 +0100

    commit

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -33,7 +33,6 @@
               </li>
               <a href="/secondary_page">
                 <button
-                  class="me-3 btn btn-danger rounded-pill"
                   style="cursor: pointer"
                 >
                   <span>Commencer la visite</span>
@@ -62,8 +61,8 @@
                 simplement transporter à travers une visite épique.
               </p>
               <a href="/secondary_page">
-                <button class="btn btn-danger rounded-pill">
-                  C'est Parti!
+                <button>
+                  <span>C'est Parti!</span>
                 </button>
               </a>
             </div>
@@ -135,6 +134,59 @@ body {
   font-size: 20px;
   padding: 10px 20px;
 }
+
+button {
+ outline: none;
+ cursor: pointer;
+ border: none;
+ padding: 0.9rem 2rem;
+ margin: 0;
+ font-family: inherit;
+ font-size: inherit;
+ position: relative;
+ display: inline-block;
+ letter-spacing: 0.05rem;
+ font-weight: 700;
+ font-size: 17px;
+ border-radius: 500px;
+ overflow: hidden;
+ background: #66ff66;
+ color: ghostwhite;
+}
+
+button span {
+ position: relative;
+ z-index: 10;
+ transition: color 0.4s;
+}
+
+button:hover span {
+ color: black;
+}
+
+button::before,
+button::after {
+ position: absolute;
+ top: 0;
+ left: 0;
+ width: 100%;
+ height: 100%;
+ z-index: 0;
+}
+
+button::before {
+ content: "";
+ background: #000;
+ width: 120%;
+ left: -10%;
+ transform: skew(30deg);
+ transition: transform 0.4s cubic-bezier(0.3, 1, 0.8, 1);
+}
+
+button:hover::before {
+ transform: translate3d(100%, 0, 0);
+}
+
 
 .women img {
   max-width: 250px;
